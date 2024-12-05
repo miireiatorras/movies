cf919bbcb61230f2bd0a381eeb39336b7d0e4c93
lucasscarmm <162101039+lucasscarmm@users.noreply.github.com>
2024-12-05 10:56:52 +0100

    buscador

--- src/features/banner/Banner.jsx
+++ src/features/banner/Banner.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Banner.css';
 
 function Banner() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/emotions'); // Redirige a la ruta "/emotions"
+  };
+
   return (
     <div className="banner-container">
       <img src="/hola.jpg" alt="banner" className="banner-img" />
       <div className="banner-content">
         <h1>Welcome to FeelReel!</h1>
         <h3>"Discover stories, feel emotion, live magic.”</h3>
-        <button>
+        <button onClick={handleButtonClick}>
           How are you feeling today?
         </button>
       </div>
@@ -17,4 +24,3 @@ function Banner() {
 }
 
 export default Banner;
-
