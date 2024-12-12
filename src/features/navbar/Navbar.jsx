b741b1e9fda86fe38e19efbe82d57b00274d45c6
miireiatorras <mireiat.mtc@gmail.com>
2024-12-12 11:37:32 +0100

    search box en el header

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <Link to="/" className="logo-link">
-            <img src="/logo_app.png" alt="logo" className="logo" />
-          </Link>
-        </div>
-        <div className="navbar-content">
-          <p className="profile-link">My profile</p>
-          <img src="/avatar.png" alt="avatar" className="avatar" />
-        </div>
-      </nav>
-    );
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchQuery.trim()) {
+      navigate(`/results?query=${searchQuery}`);
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/" className="logo-link">
+          <img src="/logo_app.png" alt="logo" className="logo" />
+        </Link>
+      </div>
+      
+      <form onSubmit={handleSearchSubmit} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies or series..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="navbar-content">
+        <p className="profile-link">My profile</p>
+        <img src="/avatar.png" alt="avatar" className="avatar" />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
